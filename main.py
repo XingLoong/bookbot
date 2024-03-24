349aaceee7c45244958a3f6280236e92200bdb37
XingLoong <xing.loong@gmail.com>
2024-03-24 18:16:47 +1100

    Finished book bot course assignments

---
+++ main.py
@@ -0,0 +1,39 @@
+def main():
+  book_path = 'books/Franky.txt'
+  text = book_text(book_path)
+  words = num_words(text)
+  print("--- Begin the book report ---")
+  print(f"{words} words in the document")
+  print("")
+  rearrange(letter_count(text))
+  print("--- End Report ---")
+
+def book_text(path):
+  with open(path) as f:
+    return f.read()
+
+def num_words(text):
+  words = text.split()
+  return len(words)
+
+def letter_count(text):
+  lowered_string = text.lower()
+  dict_count = {}
+  
+  for t in lowered_string:
+    if t.isalpha():
+      if t not in dict_count:
+        dict_count[t] = 1
+      else:
+        dict_count[t] += 1
+    else:
+      continue
+  return dict_count
+
+def rearrange(dict_count):
+  arranged = sorted(dict_count.items(), key = lambda x:x[1], reverse = True)
+  for x in arranged:
+    print(f"The '{x[0]}' character was found {x[1]} times")
+
+
+main()
